c6415773a83236bb50e251a0b9d18c7e71948023
Tristan Grovender <therealtristangrovender@gmail.com>
2019-05-03 10:42:41 -0500

    initial boilerplate complete

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/projectTracker.db3"
+  },
+  useNullAsDefault: true // needed for sqlite
+};
+
+const db = knex(knexConfig);
+
+// server
+const server = express();
+
+// Middleware
+server.use(helmet());
+server.use(express.json());
+
+// Listen on...
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+  console.log(`\n** API running on http://localhost:${port} **\n`)
+);
